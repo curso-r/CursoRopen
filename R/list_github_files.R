c7024cb86ac033dae962bf66412b03a9ee5b3f21
jtrecenti <julio.trecenti@gmail.com>
2022-03-15 08:51:46 -0300

    add pat option

--- R/list_github_files.R
+++ R/list_github_files.R
@@ -4,20 +4,37 @@
 #' @param dir nome do diretorio
 #' @param pattern padrao para buscar arquivos
 #' @param org organizacao no GitHub. Por padrao eh a curso-r.
+#' @param pat adicionar github pat como header da requisicao. Isso reduz
+#'   as chances da requisicao vir vazia. Por padrao `FALSE`.
 #'
 #' @return um vetor
 #' @export
 #'
 #' @examples list_github_files(repo = "main-web-scraping", dir = "slides/", pattern = "[0-9]-.*html$")
 
-list_github_files <- function(repo, dir = NULL, pattern = NULL, org = "curso-r") {
+list_github_files <- function(repo, dir = NULL, pattern = NULL, org = "curso-r", pat = FALSE) {
+
+  if (pat) {
+    usethis::ui_info("Tentando pegar seu pat com gitcreds::gitcreds_get()...")
+    my_pat <- gitcreds::gitcreds_get()$password
+    if (is.null(my_pat)) {
+      usethis::ui_oops("Não consegui :(")
+      usethis::ui_info("Tentando pegar seu pat Sys.getenv('GITHUB_PAT')")
+      my_pat <- Sys.getenv('GITHUB_PAT')
+      if (my_pat == "") usethis::ui_stop("Não consegui encontrar seu PAT.")
+    }
+    hh <- httr::add_headers(authorization = paste("Bearer", my_pat))
+  } else {
+    hh <- httr::add_headers()
+  }
 
   req <- httr::GET(
     paste0(
       "https://api.github.com/repos/",  org, "/",
       repo,
       "/git/trees/master?recursive=1"
-    )
+    ),
+    hh
   )
 
   arquivos <- unlist(
